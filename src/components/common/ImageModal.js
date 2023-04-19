b8cef1ad384f928b469b6240b1ec53a45dc46557
Oskar <moreez@protonmail.com>
2023-04-19 10:23:45 +0100

    bugfixes

--- src/components/common/ImageModal.js
+++ src/components/common/ImageModal.js
@@ -13,7 +13,11 @@ export default function ImageModal({ imageUrl, altText, isOpen, setIsOpen }) {
         data-aos-duration="200"
       >
         <div className="image-modal-overlay"></div>
-        <div className="image-modal-content">
+        <div
+          className="image-modal-content"
+          data-aos="zoom-in"
+          data-aos-duration="300"
+        >
           <img src={imageUrl} alt={altText} />
         </div>
       </div>

2842c8f2b6d2142b4efc6d32ccf07f61436f41c1
Oskar <moreez@protonmail.com>
2023-04-21 17:21:34 +0100

    seo open graph fix attempt

--- src/components/layout/DefaultPageLayout.jsx
+++ src/components/layout/DefaultPageLayout.jsx
@@ -29,12 +29,12 @@ export default function DefaultPageLayout({
           openGraphData,
         }}
       />
-      <Helmet>
+      {/* <Helmet>
         <link
           href="https://unpkg.com/aos@2.3.1/dist/aos.css"
           rel="stylesheet"
         />
-      </Helmet>
+      </Helmet> */}
       <Header />
       <main>{children}</main>
       <Footer />
--- src/components/layout/SEO.js
+++ src/components/layout/SEO.js
@@ -10,14 +10,11 @@ export default function SEO({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width" />
       {/* OPEN GRAPH PREVIEW TAGS */}
+      <meta name="og:title" content={openGraphData?.title || "Oskar Mroz"} />
+      <meta name="og:site_name" content="oskarmroz.com" />
+      <meta name="og:locale" content="en_US" />
       <meta
-        property="og:title"
-        content={openGraphData?.title || "Oskar Mroz"}
-      />
-      <meta property="og:site_name" content="oskarmroz.com" />
-      <meta property="og:locale" content="en_US" />
-      <meta
-        property="og:description"
+        name="og:description"
         content={
           description ||
           "Oskar Mroz is a software developer from Ireland who loves coding, UI design, photography, and blogging. He works with React.js, React Native, Node.js, and other technologies. Visit his website to see his portfolio and blog posts."
@@ -25,21 +22,21 @@ export default function SEO({
       />
 
       <meta
-        property="og:image"
+        name="og:image"
         content={
           openGraphData?.image ||
           "https://avatars.githubusercontent.com/u/47431553"
         }
       />
       <meta
-        property="og:image:alt"
+        name="og:image:alt"
         content={openGraphData?.imageAlt || "Profile picture of Oskar Mroz"}
       />
       <meta
-        property="og:url"
+        name="og:url"
         content={openGraphData?.url || "https://oskarmroz.com"}
       />
-      <meta property="og:type" content={openGraphData?.type || "website"} />
+      <meta name="og:type" content={openGraphData?.type || "website"} />
       {/* SEARCH ENGINE TAGS */}
       <title>{title || "Oskar Mroz - Software Developer from Ireland"}</title>
       <meta

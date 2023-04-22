6b3e0c8eb096b9120c65e8b3e00787d2dc2878ab
Oskar <moreez@protonmail.com>
2023-04-22 08:09:50 +0100

    added splash screen

--- src/components/common/SplashScreen.js
+++ src/components/common/SplashScreen.js
@@ -1,5 +1,16 @@
 import React from "react";
+import BeatLoader from "react-spinners/BeatLoader";
 
 export default function SplashScreen() {
-  return <div id="loader-wrapper"></div>;
+  return (
+    <div id="loader-wrapper">
+      <BeatLoader
+        color="white"
+        loading={true}
+        size={"10vw"}
+        aria-label="Loading"
+        data-testid="loader"
+      />
+    </div>
+  );
 }
--- src/components/layout/DefaultPageLayout.jsx
+++ src/components/layout/DefaultPageLayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import AOS from "aos";
-import SEO from "./SEO";
+import SplashScreen from "../common/SplashScreen";
 
 export default function DefaultPageLayout({ children }) {
   // Initialize AOS on every page.
@@ -15,6 +15,7 @@ export default function DefaultPageLayout({ children }) {
       <Header />
       <main>{children}</main>
       <Footer />
+      <SplashScreen />
     </>
   );
 }
